93c221715bb0adb90fcdb87e36524179f9fa8466
Soham Nagaokar <snagaokar6@gmail.com>
2020-07-31 16:22:56 -0400

    updated moveTo

--- Chess/src/src/Piece.java
+++ Chess/src/src/Piece.java
@@ -1,14 +1,27 @@
 package src;
 
 public abstract class Piece {
-	private boolean isWhite, isAlive;
+	private boolean isWhite, isAlive; //maybe get rid of isAlive?
 	private int points;
 	public Piece(boolean isWhite) {
 		this.isWhite = isWhite;
 		isAlive = true;
 	}
-	public abstract boolean canMove(Tile end);
-	public boolean moveTo(Tile end) {
-		return true;
+	public abstract boolean canMove(Tile start, Tile end);
+	public boolean moveTo(Tile start, Tile end) { //doesn't work for castling
+		if(canMove(start, end)) {
+			Piece captured = end.getPiece();
+			if(captured != null) {
+				captured.isAlive = false;
+				//Code to send points to main
+				captured = null;
+			}
+			end.setPiece(this);
+			start.setPiece(null);
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }

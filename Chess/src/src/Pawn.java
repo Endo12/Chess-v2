0b998fc1e37184882aa9715c9ab25023269554e5
Connor O'Meara <58488392+Vorkulux@users.noreply.github.com>
2020-07-31 16:40:59 -0400

    Update Pawn.java

--- Chess/src/src/Pawn.java
+++ Chess/src/src/Pawn.java
@@ -1,3 +1,20 @@
 public class Pawn extends Piece {
+  public boolean isWhite;
+  public int points; 
+  
+  public Pawn(boolean isWhite, int points){
+    super(isWhite, points); 
+  }
+  
+  public boolean canMove(Tile start, Tile end) {
+    if (start.piece != null && 
+        start.piece.instanceOf(Pawn) == true) { //check that it's occupied and that it's a pawn
+      
+      
+    }
+  
+  
+  
+  
   
 }

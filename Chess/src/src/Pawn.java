420aa0c2734493a2a1c6d9538826d418a289f3eb
Soham Nagaokar <snagaokar6@gmail.com>
2020-08-02 16:11:18 -0400

    Corrected stuff in pawn

--- Chess/src/src/Pawn.java
+++ Chess/src/src/Pawn.java
@@ -1,27 +1,30 @@
+package src;
+
 public class Pawn extends Piece {
   
-  public Pawn(boolean isWhite, int points){
-    super(isWhite, points); 
+  public Pawn(boolean isWhite){
+    super(isWhite, 1); 
   }
   
   public boolean canMove(Tile start, Tile end) {
-    if (start.piece != null && && end.getX() != start.getX() { //check that it's occupied and that start and end are not the same tile 
-      if (isWhite) { // a forward move for white is +1 in y direction, right is +1 in x
-        if (end.getY() == start.getY() + 1 && end.piece == null) { //if forward, it must not occupied by ANY piece
+    if (start.equals(end)) { //check that start and end are not the same tile 
+      if (this.getColor()) { // a forward move for white is +1 in y direction, right is +1 in x
+        if (end.getY() == start.getY() + 1 && end.getPiece() == null) { //if forward, it must not occupied by ANY piece
           return true; 
         }
-        else if (end.getY() == start.getY() + 1 && (end.getX() == start.getX() - 1 || end.getX() == start.getX() + 1) //piece tries forward one, and left one/right one (diagonal)
-               && !end.piece.getColor()) { //if diagional, it must be occupied by black piece
+        //piece tries forward one, and left one/right one (diagonal)
+        else if (end.getY() == start.getY() + 1 && (end.getX() == start.getX() - 1 || end.getX() == start.getX() + 1) 
+               && !end.getPiece().getColor()) { //if diagional, it must be occupied by black piece
           return true; 
         } else {
           return false; 
         }
       } else { //confirmed black pawn, whose "forward" is -1 in y and whose right is -1 in x 
-        if (end.getY() == start.getY() - 1 && end.piece == null) { //if forward, it must not occupied by ANY piece
+        if (end.getY() == start.getY() - 1 && end.getPiece() == null) { //if forward, it must not occupied by ANY piece
           return true; 
         }
         else if (end.getY() == start.getY() - 1 && (end.getX() == start.getX() + 1 || end.getX() == start.getX() - 1) //piece tries forward one, and left one/right one (diagonal)
-               && end.piece.getColor()) { //if diagional, it must be occupied by WHITE piece
+               && end.getPiece().getColor()) { //if diagional, it must be occupied by WHITE piece
           return true; 
         } else {
           return false; 
@@ -31,9 +34,5 @@ public class Pawn extends Piece {
    } else {
          return false;  
       }
-  
-  
-  
-  
-  
+  }
 }

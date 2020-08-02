5c8853b43fae4ef5e330f409ce5e9aa7ff6fac3d
Connor O'Meara <58488392+Vorkulux@users.noreply.github.com>
2020-08-02 16:28:50 -0400

    Update Bishop.java

--- Chess/src/src/Bishop.java
+++ Chess/src/src/Bishop.java
@@ -10,26 +10,26 @@ public class Bishop extends Piece {
     if (end.getPiece() == null || this.getColor() != end.getPiece().getColor() && !start.equals(end)) { 
     	int diffX = Math.abs(start.getX() - end.getX()); 
     	int diffY = Math.abs(start.getY() - end.getY()); 
-      int tempX = start.getX(); //used to check 
-      int tempY = start.getY(); 
-      if (diffX == diffY) { //the proposed move is diagonal, now check for collisions before move can be completed 
-          Tile[][] tiles = Board.tileBoard; //retrieve tileBoard from the Board class
-    	  while (tempX != end.getX() && tempY != end.getY()) { //check each tile in the diagonal for a collision before the end is reached
-           if (start.getX() < end.getX()) { //we know we increment x for diagonal
-            tempX++; 
-           } else { //we decrement x for diagonal
-            tempX--;  
-           }
-          if (start.getY() < end.getY()) { //we know we increment y for diagonal 
-            tempY++;  
-          } else { //we decrement y for diagonal 
-            tempY--;  
-          }
-           if (tempX != end.getX() && tempY != end.getY() && tiles[tempY][tempX].getPiece() != null) { // if we haven't completed our diagonal check and we found a piece in the way
-             return false;
-           }
-         } // diagonal check completed
-        return true; 
+        int tempX = start.getX(); //used to check 
+        int tempY = start.getY(); 
+        if (diffX == diffY) { //the proposed move is diagonal, now check for collisions before move can be completed 
+        	Tile[][] tiles = Board.tileBoard; //retrieve tileBoard from the Board class
+    		while (tempX != end.getX() && tempY != end.getY()) { //check each tile in the diagonal for a collision before the end is reached
+        		if (start.getX() < end.getX()) { //we know we increment x for diagonal
+            	tempX++; 
+           		} else { //we decrement x for diagonal
+            		tempX--;  
+           		}
+          		if (start.getY() < end.getY()) { //we know we increment y for diagonal 
+           	 		tempY++;  
+          		} else { //we decrement y for diagonal 
+            		tempY--;  
+          		}
+           		if (tempX != end.getX() && tempY != end.getY() && tiles[tempY][tempX].getPiece() != null) { // if we haven't completed our diagonal check and we found a piece in the way
+             	return false;
+           		}
+         	} // diagonal check completed
+      		return true; 
       } else { //move not diagonal 
         return false;  
       }

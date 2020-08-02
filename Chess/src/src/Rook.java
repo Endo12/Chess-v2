9b4c736d01e8277d8dd0f58ce565b01d38d40e34
Soham Nagaokar <snagaokar6@gmail.com>
2020-08-02 15:53:26 -0400

    Implemented canMove

--- Chess/src/src/Rook.java
+++ Chess/src/src/Rook.java
@@ -9,7 +9,8 @@ public class Rook extends Piece {
 	@Override
 	public boolean canMove(Tile start, Tile end) {
 		Piece destPiece = end.getPiece();
-		boolean diffX = start.getX() != end.getX(), diffY = start.getY() != end.getY();
+		int startX = start.getX(), endX = end.getX(), startY = start.getY(), endY = end.getY();
+		boolean diffX = startX != endX, diffY = startY != endY;
 		if((destPiece != null && destPiece.getColor() == this.getColor()) || start.equals(end) || 
 				diffX && diffY) { /*Condition ensures that the piece wouldn't be capturing another 
 				piece on its team, that the 2 tiles are different, and that the 2 tiles share a row 
@@ -17,7 +18,21 @@ public class Rook extends Piece {
 			return false;
 		}
 		Tile boardArr[][] = Board.tileBoard;
-		
+		if(diffX) { /*Cycles through X positions and returns false if there's any collisions*/
+			for(int f=Math.min(startX, endX); f<Math.max(startX, endX); f++) {
+				if(boardArr[startY][f].getPiece() != null) {
+					return false;
+				}
+			}
+		}
+		else { /*Same as above, but with Y positions*/
+			for(int f=Math.min(startY, endY); f<Math.max(startY, endY); f++) {
+				if(boardArr[f][startX].getPiece() != null) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 }

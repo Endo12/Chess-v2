c66582a14dc65d31cd09c4ec811793e8218e362a
Connor O'Meara <58488392+Vorkulux@users.noreply.github.com>
2020-08-02 15:26:43 -0400

    Update Bishop.java

--- Chess/src/src/Bishop.java
+++ Chess/src/src/Bishop.java
@@ -1,3 +1,4 @@
+import java.lang.Math.abs;
 public class Bishop extends Piece {
 
   public Bishop(boolean isWhite, int points) {
@@ -6,7 +7,33 @@ public class Bishop extends Piece {
   
   public boolean canMove(Tile start, Tile end) {
      // checklist: different start/end, that the move is "diagonal", that it avoids collisions along the way, and that when it does collide, it's w/ opposite color
-    boolean isWhite = this.getColor(); //
+    boolean thisWhite = this.getColor(); 
+    boolean moveIsDiagonal = true; 
+    boolean avoidsCollisions = true; 
+    
+    if (!end.getPiece() && this.getColor != end.getPiece().getColor() && start != end) { // can only move to a tile with a different color piece or an empty tile (start/end are different) 
+      int diffX = Math.abs(start.getX(), end.getX()); 
+      int diffY = Math.abs(start.getY(), end.getY()); 
+      int tempX = start.getX(); 
+      int tempY = start.getX(); 
+      if (diffX == diffY) { //the proposed move is diagonal, now check for collisions before move can be completed 
+         while (tempX != end.getX() && tempY != end.getY()) {
+           Tile temp = 
+           if (start.getX() < end.getX()) {
+            tempX++; 
+           } else {
+              tempX--;  
+           }
+          if (start.getY() < end.getY()) {
+            tempY++;  
+          } else {
+            tempY--;  
+          }
+         }
+      }
+    } else {
+      return false;  
+    }
     
     
     

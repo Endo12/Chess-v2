02e564f5cd35da6e4eb36df9150d33b116f3bf5c
Connor O'Meara <58488392+Vorkulux@users.noreply.github.com>
2020-08-02 14:42:00 -0400

    started barebones of bishop

--- Chess/src/src/Bishop.java
+++ Chess/src/src/Bishop.java
@@ -1,8 +1,15 @@
 public class Bishop extends Piece {
 
-public Bishop(boolean isWhite, int points) {
-  super(isWhite, points); 
-}
+  public Bishop(boolean isWhite, int points) {
+    super(isWhite, points); 
+  }
+  
+  public boolean canMove(Tile start, Tile end) {
+     
+  }
+  
+
+  
 
 
 

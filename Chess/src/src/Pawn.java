7aeb164bd58570b9834ca148a58a91ab536752a1
Connor O'Meara <58488392+Vorkulux@users.noreply.github.com>
2020-08-02 14:30:03 -0400

    Update Pawn.java

--- Chess/src/src/Pawn.java
+++ Chess/src/src/Pawn.java
@@ -6,11 +6,18 @@ public class Pawn extends Piece {
   
   public boolean canMove(Tile start, Tile end) {
     if (start.piece != null && 
-        start.getPiece() instanceof Pawn && !(end.getX() == start.getX()) { //check that it's occupied and that it's a pawn
-      if (end.getY() == start.getY() + 1 && end.piece == null) { //if forward, not occupied
-      else if (end.getY() == start.getY() + 1 && end.getX() == start.getX() + 1 && end.piece != null) {
-        
+        start.getPiece() instanceof Pawn && end.getX() != start.getX() { //check that it's occupied and that it's a pawn, and that the "end" requires movement
+      
+      if (isWhite) {
+        if (end.getY() == start.getY() + 1 && end.piece == null) { //if forward, it must not occupied by ANY piece
+          return true; 
       }
+      else if (end.getY() == start.getY() + 1 && (end.getX() == start.getX() + 1 || end.getX() == start.getX() - 1) 
+               && end.piece.) { //if diagional, it must be occupied by black piece
+        
+      } 
+      }
+      
       
     }
   

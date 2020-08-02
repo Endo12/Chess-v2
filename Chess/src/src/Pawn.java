1de513eae6f5036785fc062c110f4f7513191e4d
Connor O'Meara <58488392+Vorkulux@users.noreply.github.com>
2020-08-02 14:49:38 -0400

    Update Pawn.java

--- Chess/src/src/Pawn.java
+++ Chess/src/src/Pawn.java
@@ -5,9 +5,7 @@ public class Pawn extends Piece {
   }
   
   public boolean canMove(Tile start, Tile end) {
-    if (start.piece != null && 
-        start.getPiece() instanceof Pawn && end.getX() != start.getX() { //check that it's occupied and that it's a pawn, and that the "end" requires movement
-      
+    if (start.piece != null && && end.getX() != start.getX() { //check that it's occupied and that start and end are not the same tile 
       if (isWhite) { // a forward move for white is +1 in y direction, right is +1 in x
         if (end.getY() == start.getY() + 1 && end.piece == null) { //if forward, it must not occupied by ANY piece
           return true; 

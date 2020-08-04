ac4af23ac0e104dc686edf853bc2ae7ce585be8c
Connor O'Meara <58488392+Vorkulux@users.noreply.github.com>
2020-08-03 21:15:06 -0400

    Create Horse.java

---
+++ Chess/src/src/Horse.java
@@ -0,0 +1,18 @@
+public class Horse extends Piece {
+
+public Horse(boolean isWhite) {
+	super(isWhite); 
+}
+
+public boolean canMove(Tile start, Tile end) {
+	int diffX = Math.abs(start.getX() - end.getX()); // requested horizontal distance of move
+	int diffY = Math.abs(start.getY() - end.getY()); // requested vertical distance of move
+	if ((diffX == 2 && diffY == 1) || (diffY == 2 && diffX == 1)) { // knight can move 2 horizontal and 1 vertical, or 1 horizontal and 2 vertical 
+		if (end.getPiece() == null || end.getPiece().getColor() != this.getColor()) { // can move as long as the end tile is empty or has a different color piece
+			return true; 
+		}
+	} 
+	return false; 
+}
+
+}

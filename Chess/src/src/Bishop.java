dc7a1c78eacaaaa685de8bac8fc1e70be17a0d99
Soham Nagaokar <snagaokar6@gmail.com>
2020-08-03 20:26:35 -0400

    Added Queen, implemented Queen's moveTo, streamlined code

--- Chess/src/src/Bishop.java
+++ Chess/src/src/Bishop.java
@@ -8,29 +8,22 @@ public class Bishop extends Piece {
 	public boolean canMove(Tile start, Tile end) {
     // can only move to a tile with a different color piece or an empty tile (start/end are different) 
 		if (end.getPiece() == null || this.getColor() != end.getPiece().getColor() && !start.equals(end)) { 
-	    	int diffX = Math.abs(start.getX() - end.getX()); 
-	    	int diffY = Math.abs(start.getY() - end.getY()); 
-	    	int tempX = start.getX(); //used to check 
-	    	int tempY = start.getY(); 
+	    	int diffX = Math.abs(start.getX() - end.getX()), diffY = Math.abs(start.getY() - end.getY()), 
+	    			tempX = start.getX(), tempY = start.getY(); 
 	    	if (diffX == diffY) { //the proposed move is diagonal, now check for collisions before move can be completed 
 	    		Tile[][] tiles = Board.tileBoard; //retrieve tileBoard from the Board class
 	    		while (tempX != end.getX() && tempY != end.getY()) { //check each tile in the diagonal for a collision before the end is reached
-	    			if (start.getX() < end.getX()) { //we know we increment x for diagonal
-	    				tempX++; 
-	    			} else { //we decrement x for diagonal
-	    				tempX--;  
-	    			}
-	    			if (start.getY() < end.getY()) { //we know we increment y for diagonal 
-	    				tempY++;  
-	    			} else { //we decrement y for diagonal 
-	    				tempY--;  
-	    			}
-	    			if (tempX != end.getX() && tempY != end.getY() && tiles[tempY][tempX].getPiece() != null) { // if we haven't completed our diagonal check and we found a piece in the way
+	    			tempX += start.getX() < end.getX() ? 1 : -1; //changes X for diagonal
+	    			tempY += start.getY() < end.getY() ? 1 : -1; //changes Y for diagonal
+	    			if (tempX != end.getX() && tempY != end.getY() && tiles[tempY][tempX].getPiece() 
+	    					!= null) { /* if we haven't completed our diagonal check and we found a 
+	    					piece in the way*/
 	    				return false;
 	    			}
 	    		} // diagonal check completed
 	    		return true; 
-	    	} else { //move not diagonal 
+	    	} 
+	    	else { //move not diagonal 
 	    		return false;  
 	    	}
 		} else {
---
+++ Chess/src/src/Queen.java
@@ -0,0 +1,55 @@
+package src;
+
+public class Queen extends Piece {
+
+	public Queen(boolean isWhite) {
+		super(isWhite, 9);
+	}
+
+	@Override
+	public boolean canMove(Tile start, Tile end) {
+		Piece destPiece = end.getPiece();
+		/*Checks to make sure tiles are different, and that the queen wouldn't be capturing a piece of
+		her own color*/
+		if(!start.equals(end) && (destPiece == null || destPiece.getColor() != this.getColor()) ) {
+			Tile boardArr[][] = Board.tileBoard;
+			int currX = start.getX(), endX = end.getX(), currY = start.getY(), endY = end.getY(), 
+					diffX = Math.abs(start.getX() - end.getX()), diffY = Math.abs(start.getY() - end.getY()); 
+			if(diffX == 0 && diffY != 0) { /*Cycles through X positions and returns false if there's 
+				any collisions*/
+				for(int f=Math.min(currX, endX); f<Math.max(currX, endX); f++) {
+					if(boardArr[currY][f].getPiece() != null) {
+						return false;
+					}
+				}
+			}
+			else if(diffX != 0 && diffY == 0) { /*Same as above, but with Y positions*/
+				for(int f=Math.min(currY, endY); f<Math.max(currY, endY); f++) {
+					if(boardArr[f][currX].getPiece() != null) {
+						return false;
+					}
+				}
+			}
+			else if(diffX == diffY) { /*the proposed move is diagonal, now check for collisions before 
+				move can be completed*/
+	    		while (currX != end.getX() && currY != end.getY()) { /*check each tile in the diagonal 
+	    			for a collision before the end is reached*/
+	    			currX += start.getX() < end.getX() ? 1 : -1; //changes X for diagonal
+	    			currY += start.getY() < end.getY() ? 1 : -1; //changes Y for diagonal
+	    			if (currX != end.getX() && currY != end.getY() && boardArr[currY][currX].getPiece() 
+	    					!= null) { /* if we haven't completed our diagonal check and we found a 
+	    					piece in the way*/
+	    				return false;
+	    			}
+	    		} // diagonal check completed
+			}
+			else { //proposed move is neither straight nor diagonal
+				return false;
+			}
+	    	return true; 
+		}
+		else {
+			return false;
+		}
+	}
+}

d56901f62224e5bacc8612e7ae094fdf8467d55b
Connor O'Meara <58488392+Vorkulux@users.noreply.github.com>
2020-08-03 21:32:51 -0400

    King done, minus "can't move into check" logic

--- Chess/src/src/King.java
+++ Chess/src/src/King.java
@@ -1,1 +1,19 @@
+public class King extends Piece {
+	
+	public King(boolean isWhite) {
+		super(isWhite); 	
+	}
+	
+	public boolean canMove(Tile start, Tile end) {
+		int diffX = Math.abs(start.getX() - end.getX()); 
+		int diffY = Math.abs(start.getY() - end.getY()); 
+		if (diffX + diffY <= 2 && diffX < 2 && diffY < 2) { // can only move one space in vertical direction or horizontal direction, or one in both directions
+			if (end.getPiece() == null || this.getColor() != end.getPiece().getColor()) { // check for empty space or occupied by enemy piece
+				for(
+			}	
+		}
+		
+	}
+	
 
+}

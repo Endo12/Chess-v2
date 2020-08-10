c8e23a06ac948188bd443352afb9c2c445d9fbec
Soham Nagaokar <snagaokar6@gmail.com>
2020-08-09 22:26:37 -0400

    Constructor fixes

--- Chess/src/src/King.java
+++ Chess/src/src/King.java
@@ -1,7 +1,9 @@
+package src;
+
 public class King extends Piece {
 	
-	public King(boolean isWhite, int points) {
-		super(isWhite, points); 	
+	public King(boolean isWhite) {
+		super(isWhite, 0); 	
 	}
 	
 	public boolean canMove(Tile start, Tile end) {
@@ -10,10 +12,12 @@ public class King extends Piece {
 		if (diffX + diffY <= 2 && diffX < 2 && diffY < 2) { // can only move one space in vertical direction or horizontal direction, or one in both directions
 			if (end.getPiece() == null || this.getColor() != end.getPiece().getColor()) { // check for empty space or occupied by enemy piece
 				Tile[][] tiles = Board.tileBoard; 
-				for(Tile t: tiles) { // check every tile on the board...
-					if (t.getPiece().getColor() != this.getColor()) { //if the tile has an enemy piece 
-						if (t.getPiece().canMove(t, end)) { // and the piece can move to "Tile end"
-							return false; // the move would put king in check, can't move to "Tile end"
+				for(Tile[] r: tiles) { // check every tile on the board...
+					for(Tile t: r) {
+						if (t.getPiece().getColor() != this.getColor()) { //if the tile has an enemy piece 
+							if (t.getPiece().canMove(t, end)) { // and the piece can move to "Tile end"
+								return false; // the move would put king in check, can't move to "Tile end"
+							}
 						}
 					}
 				} 
--- Chess/src/src/Knight.java
+++ Chess/src/src/Knight.java
@@ -1,17 +1,19 @@
+package src;
+
 public class Knight extends Piece {
 
-public Horse(boolean isWhite, int points) {
-	super(isWhite, points); 
-}
-
-public boolean canMove(Tile start, Tile end) {
-	int diffX = Math.abs(start.getX() - end.getX()); // requested horizontal distance of move
-	int diffY = Math.abs(start.getY() - end.getY()); // requested vertical distance of move
-	if ((diffX == 2 && diffY == 1) || (diffY == 2 && diffX == 1)) { // knight can move 2 horizontal and 1 vertical, or 1 horizontal and 2 vertical 
-		if (end.getPiece() == null || end.getPiece().getColor() != this.getColor()) { // can move as long as the end tile is empty or has a different color piece
-			return true; 
-		}
-	} 
-	return false; 
-}
+	public Knight(boolean isWhite) {
+		super(isWhite, 3); 
+	}
+	
+	public boolean canMove(Tile start, Tile end) {
+		int diffX = Math.abs(start.getX() - end.getX()); // requested horizontal distance of move
+		int diffY = Math.abs(start.getY() - end.getY()); // requested vertical distance of move
+		if ((diffX == 2 && diffY == 1) || (diffY == 2 && diffX == 1)) { // knight can move 2 horizontal and 1 vertical, or 1 horizontal and 2 vertical 
+			if (end.getPiece() == null || end.getPiece().getColor() != this.getColor()) { // can move as long as the end tile is empty or has a different color piece
+				return true; 
+			}
+		} 
+		return false; 
+	}
 }

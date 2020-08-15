19cf49613548799f950f1a80c2947ccd49b2a460
Connor O'Meara <58488392+Vorkulux@users.noreply.github.com>
2020-08-15 16:57:53 -0400

    Update TableGUI.java

--- Chess/src/src/TableGUI.java
+++ Chess/src/src/TableGUI.java
@@ -1,20 +1,27 @@
-package src;
+import javafx.application.Application;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener; 
+import javafx.scene.*;
+import javafx.stage.Stage;
 
-public class TableGUI {
-	private final JFrame game; 
-	
-	public TableGUI() {
-		game = new JFrame("Chess Engine"); 
-		this.game.setSize(new Dimension(800, 800)); 
-		this.game.setVisible(true); 
-	}
+public class ChessGUI {
+    GridPane gPane = new GridPane();
 
-
+    public void start(Stage primaryStage) {
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 8; c++) {
+                Rectangle tile = new Rectangle();
+                Color color;
+                if ((r) % 2 == 0) color = Color.WHITE;
+                else color = Color.BLACK;
+                square.setFill(color);
+                root.add(tile, c, r);
+                tile.widthProperty().bind(gPane.widthProperty().divide(8));
+                tile.heightProperty().bind(gPane.heightProperty().divide(8));
+            }
+        }
+        primaryStage.setScene(new Scene(gPane, 600, 600));
+        primaryStage.show();
+    }
 
 
 

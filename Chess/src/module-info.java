eefc3a62882a097804157eb9563dbf956dc6d98d
Soham Nagaokar <snagaokar6@gmail.com>
2020-08-15 17:38:24 -0400

    Fixed JavaFX stuff

--- Chess/src/module-info.java
+++
@@ -1,4 +0,0 @@
-module Chess {
-	requires java.desktop;
-	requires javafx.graphics;
-}
--- Chess/src/src/ChessGUI.java
+++ Chess/src/src/ChessGUI.java
@@ -1,16 +1,14 @@
 package src;
 
 import javafx.application.Application;
-
-import javafx.scene.*;
+import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-public class ChessGUI {
-    GridPane gPane = new GridPane();
-
+public class ChessGUI extends Application{
+	GridPane gPane = new GridPane();
     public void start(Stage primaryStage) {
         for (int r = 0; r < 8; r++) {
             for (int c = 0; c < 8; c++) {
@@ -27,17 +25,11 @@ public class ChessGUI {
         primaryStage.setScene(new Scene(gPane, 600, 600));
         primaryStage.show();
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Application.launch(args);
+	}
 }
+
+

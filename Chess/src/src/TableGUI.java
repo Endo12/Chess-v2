105c04eb23cf6c229c4cc2357b3c1f2e35c843fe
Connor O'Meara <58488392+Vorkulux@users.noreply.github.com>
2020-08-15 15:50:06 -0400

    Create TableGUI.java

---
+++ Chess/src/src/TableGUI.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener; 
+
+public class TableGUI {
+	private final JFrame game; 
+	
+	public TableGUI() {
+		game = new JFrame("Chess Engine"); 
+		this.game.setSize(new Dimension(800, 800)); 
+		this.game.setVisible(true); 
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

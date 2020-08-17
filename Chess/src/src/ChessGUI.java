b61f3a4a4600f97f4d73a1ebf7a679c27e61b13e
Soham Nagaokar <snagaokar6@gmail.com>
2020-08-16 21:08:13 -0400

    updateBoard is now responsible for board and piece creation, white pawns
    show up correctly

--- Chess/src/src/ChessGUI.java
+++ Chess/src/src/ChessGUI.java
@@ -20,48 +20,38 @@ public class ChessGUI extends Application{
 	GridPane gPane = new GridPane();
     public void start(Stage primaryStage) {
         Board b = new Board();
-        for (int r = 0; r < 8; r++) {
-            for (int c = 0; c < 8; c++) {
-                Rectangle tile = new Rectangle();
+        updateBoard(gPane);
+        primaryStage.setTitle("Chess");
+        primaryStage.setScene(new Scene(gPane, 815, 815));
+        primaryStage.show();
+    }
+    public void updateBoard(GridPane gPane) { //Creates visual chessboard and places pieces
+		Tile[][] board = Board.tileBoard;
+    	for(int r=0; r<8; r++) {
+    		for(int c=0; c<8; c++) {
+    			Rectangle tile = new Rectangle();
                 Color color = r % 2 == c % 2 ? Color.WHITE : Color.BLACK;
                 tile.setFill(color);
                 tile.widthProperty().bind(gPane.widthProperty().divide(8));
                 tile.heightProperty().bind(gPane.heightProperty().divide(8));
                 StackPane stack = new StackPane();
-                ImageView imv = new ImageView(new 
-                		Image("https://static.thenounproject.com/png/722241-200.png"));
-                imv.setFitHeight(100);
-				imv.setFitWidth(100);
-                stack.getChildren().addAll(tile, imv);
-                gPane.add(stack, r, c);
-                
-            }
-        }
-        //updateBoard(gPane);
-        primaryStage.setTitle("Chess");
-        primaryStage.setScene(new Scene(gPane, 815, 815));
-        primaryStage.show();
-    }
-    public void updateBoard(GridPane gPane) {
-		Tile[][] board = Board.tileBoard;
-    	for(int r=0; r<8; r++) {
-    		for(int c=0; c<8; c++) {
-    			Piece p = board[r][c].getPiece();
+                stack.getChildren().add(tile);
+    			Piece p = board[c][r].getPiece();
     			if(p != null) {
     				String link = "";
-    				Group g = null;
     				if(p.toString().equals("WP")) {
-    					link = "https://static.thenounproject.com/png/722241-200.png";
+    					link = "https://raw.githubusercontent.com/jlundstedt/chess-java/master/resources/wpawn.png";	
     				}
     				else {
-    					continue;
+    					//ignore this link connor, its a placeholder
+    					link = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Ffree-png-ybwmn&psig=AOvVaw1fzpTf4JTAPbIrPOldnemE&ust=1597712498054000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNjD6oOFoesCFQAAAAAdAAAAABAD";
     				}
     				ImageView imv = new ImageView(new Image(link));
     				imv.setFitHeight(100);
     				imv.setFitWidth(100);
-					g = new Group(new HBox(imv));
-    				gPane.add(g, c, r);
+    				stack.getChildren().add(imv);
     			}
+                gPane.add(stack, r, c);
     		}
     	}
     }

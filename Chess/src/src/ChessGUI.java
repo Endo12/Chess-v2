2bdc3710c75704776a8c8219ba10557227da5423
Connor O'Meara <58488392+Vorkulux@users.noreply.github.com>
2020-08-18 18:55:34 -0400

    got rid of inner thread class, making it its own

--- Chess/src/src/ChessGUI.java
+++ Chess/src/src/ChessGUI.java
@@ -194,26 +194,7 @@ public class ChessGUI extends Application {
     		}
 		}
     }
-    private static class ThreadHandler extends Thread {
-    	private boolean frontend;
-    	private String[] args;
-    	private ChessGUI chess;
-    	public ThreadHandler (boolean frontend, String[] args, ChessGUI chess) {
-    		this.frontend = frontend;
-    		this.args = args;
-    		this.chess = chess;
-    	}
-		@Override
-		public void run() {
-			if(frontend) {
-				Application.launch(args);
-			}
-			else {
-				chess.playGame();
-			}
-		}
-    	
-    }
+    
 }
 
 
